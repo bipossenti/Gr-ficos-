2ae4f1e9ea5b14c73f43b598f727ef00f600b82c
bianca possenti <107114103+bipossenti@users.noreply.github.com>
2024-09-27 09:32:24 -0300

    Update quantidadeUsuarios.js

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -23,7 +23,7 @@ async function quantidadeUsuariosPorRede() {
         paper_bgcolor: getCSS('--bg-color')
 
     }
-
+    {
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
