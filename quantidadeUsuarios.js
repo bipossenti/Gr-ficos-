c8e44e4fedc2d034d0ca1476f34b505854e4f3d1
bianca possenti <107114103+bipossenti@users.noreply.github.com>
2024-09-27 08:29:39 -0300

    Atualizar o quantidadeUsuarios.js

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -22,7 +22,7 @@ async function quantidadeUsuariosPorRede() {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'Redes sociais com mais usuários',
+            text: 'Redes sociais com mais usuários no mundo',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
